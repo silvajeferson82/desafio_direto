e1885b1d016a4682ab685cb7484dac1a560fa810
Jeferson SIlva <34014937+silvajeferson82@users.noreply.github.com>
2021-05-09 17:26:44 -0300

    Atualização codigo

--- src/App.js
+++ src/App.js
@@ -1,42 +1,17 @@
-import React, { Component} from 'react'
+import React, { Component} from 'react';
+import {BrowserRouter} from 'react-router-dom';
 import Routes from './routes'
 
 
 function App(){
-  /*
-  state = {
-    devedores: [],
-  }
-
-  componentDidMount(){
-    fetch(api.get(''))
-    .then(response  => response.json())
-    .then((data) => console.log(data))
-  }
-
-    const { devedores } = this.state;
-    console.log(devedores);
-    console.log(devedores.DadosListTitulos);*/
 
     return(
       <div>
         <h1>Lista Devedores</h1>
-          {/* {devedores && devedores.DadosListTitulos.length > 0 && (
-
-          )}
-          {!devedores && devedores.DadosListTitulos.length === 0 && (
-            <h1>Dados nao encontrados</h1>
-          )}
-         {devedores.DadosListTitulos.map(devedor => (
-           <li key={devedor.DadosListTitulos.dtr_id}>
-              <h2>
-                <strong>Nome</strong>
-                {devedor.dtr_nomedevedor}
-                {devedor.dtr_status}
-                {devedor.dtr_estadodevedor}
-              </h2>
-           </li>
-          ))} */}
+        <BrowserRouter>
+          <Routes/>
+        </BrowserRouter>
+          
       </div>
      
     )
--- src/pages/listar/index.js
+++ src/pages/listar/index.js
@@ -1,8 +1,26 @@
-import React from 'react'
-import api from './../../services/api'
+import React, { useState, useEffect } from "react";
+import Api from "./../../services/api";
 
-function Listar(){
+function Listar() {
+  const [devedores, setDevedores] = useState([]);
 
+  useEffect(() => {
+    Api.get("/", async (req, res) => {
+      try {
+        const { data } = await Api;
+        console.log(data);
+        return res.json(data);
+      } catch (error) {
+        console.log(error);
+      }
+    });
+  });
+
+  return (
+    <>
+      <h2>Teste de chamada API</h2>
+    </>
+  );
 }
 
-export default Listar
+export default Listar;
